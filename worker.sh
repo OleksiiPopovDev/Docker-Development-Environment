398b5ed03d1bcfa9555819268c4482d0db1a813a
Aleksey Popov <popov.scar@gmail.com>
2020-12-20 21:31:08 +0200

    Docker Setup

--- worker.sh
+++ worker.sh
@@ -1,7 +1,20 @@
-cp .env.example .env
+if [ "$1" = "monitor" ]; then
+  sampler --config ./docker/sampler/config.yml
+fi
 
-echo "\n" >> .env
-echo "USERNAME=$(whoami)" >> .env
-echo "USERID=$(id -u $(whoami))" >> .env
+if [ "$1" = "bash" ]; then
+  docker exec -it AlekseyPopov-Dev-Env-api /bin/bash
+fi
 
-docker-compose up --build
+if [ "$1" = "install" ]; then
+  cp .env.example .env
+
+  # shellcheck disable=SC2129
+  # shellcheck disable=SC2028
+  echo "\n" >> .env
+  echo "USERNAME=$(whoami)" >> .env
+  # shellcheck disable=SC2046
+  echo "USERID=$(id -u $(whoami))" >> .env
+
+  docker-compose up --build
+fi
