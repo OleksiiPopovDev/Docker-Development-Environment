346ed6479409f97a9c2cea479cd699c60d402d10
Aleksey Popov <popov.scar@gmail.com>
2020-12-25 17:28:49 +0200

    Bash: Auto installer

--- worker.sh
+++ worker.sh
@@ -1,62 +1,121 @@
 #!/bin/bash
 
-if [ "$1" = "monitor" ]; then
-  sampler --config ./docker/sampler/config.yml
-fi
-
-if [ "$1" = "bash" ]; then
-  docker exec -it $(grep CONTAINER_NAME_API .env | cut -d '=' -f2) /bin/bash
-fi
-
-if [ "$1" = "start" ]; then
-  docker-compose up --build
-fi
-
-if [ "$1" = "test" ]; then
-  echo "List of new projects:"; echo;
-
+newProjectsList() {
   COUNTER=0;
-  for d in $(cat .repositories); do
-    echo $d
-  done
-  for d in api/*/; do
-    #cp "$d"docker/nginx.conf docker/sites/$(echo $d | cut -d '/' -f2)-test.conf
-    #PROJECT_NAME=$(echo $d | cut -d '/' -f2 | awk '{print tolower($0)}')
-    FILE=docker/sites/$PROJECT_NAME-test.conf
+  for repository in $(cat .repositories); do
+    PROJECT_NAME=$(echo $repository | cut -d '/' -f5 | cut -d '.' -f1)
+    FILE_NAME=$(echo $repository | cut -d '/' -f5 | cut -d '.' -f1 | awk '{print tolower($0)}')
+    FILE_CONF=docker/sites/$FILE_NAME.conf
 
-    if [ ! -f "$FILE" ]; then
-      echo $(echo $d | cut -d '/' -f2)" - [$COUNTER]"
+    if [ ! -f "$FILE_CONF" ]; then
+      echo "[$COUNTER] - "$PROJECT_NAME
       COUNTER=$(( COUNTER + 1 ))
     fi
   done
+
+  if [ $COUNTER = 0 ]; then
+    echo "No unidentified projects!"
+    echo "Add repository url to file .repositories for new projects!"
+    exit
+  fi
+}
+
+oldProjectsList() {
+  COUNTER=0;
+  for project in api/*; do
+    PROJECT_NAME=$(echo $project | cut -d '/' -f2 )
+    FILE_NAME=$(echo $PROJECT_NAME | awk '{print tolower($0)}')
+    FILE_CONF=docker/sites/$FILE_NAME.conf
+
+    if [ -f "$FILE_CONF" ]; then
+      echo "[$COUNTER] - "$PROJECT_NAME
+      COUNTER=$(( COUNTER + 1 ))
+    fi
+  done
+
+  if [ $COUNTER = 0 ]; then
+    echo "No identified projects!"
+    echo "Install project before uninstalling!"
+    exit
+  fi
+}
+
+[ "$1" = "monitor" ] && sampler --config ./docker/sampler/config.yml
+[ "$1" = "bash" ] && docker exec -it $(grep CONTAINER_NAME_API .env | cut -d '=' -f2) /bin/bash
+[ "$1" = "up" ] && docker-compose up --build
+[ "$1" = "start" ] && docker-compose start
+[ "$1" = "stop" ] && docker-compose stop
+
+if [ "$1" = "install" ]; then
+  echo "List of new projects:"; echo;
+
+  newProjectsList
 
   echo; echo "Choose number of project for installation:"
   read CHOOSED_PROJECT
 
   COUNTER=0;
-  for d in api/*/; do
+  for repository in $(cat .repositories); do
     if [ $COUNTER = $CHOOSED_PROJECT ]; then
-      PROJECT_NAME=$(echo $d | cut -d '/' -f2 | awk '{print tolower($0)}')
-      FILE=docker/sites/$PROJECT_NAME-test.conf
+      docker-compose down
 
-      echo;
-      echo "Choosed project: "$PROJECT_NAME
-      echo "Copy conf file: "$FILE
+      PROJECT_FOLDER=$(echo $repository | cut -d '/' -f5 | cut -d '.' -f1)
+      FILE_NAME=$(echo $repository | cut -d '/' -f5 | cut -d '.' -f1 | awk '{print tolower($0)}')
+
+      DOCKER_FILE_CONF=docker/sites/$FILE_NAME.conf
+      PROJECT_FILE_CONF=api/$PROJECT_FOLDER/docker/nginx.conf
+
+      [ ! -d "api/" ] && mkdir api/
+      [ ! -d "docker/sites/" ] && mkdir docker/sites/
+
+      cd api/ && git clone $repository && cd ../
+
+      [ -f "$PROJECT_FILE_CONF" ] && cp $PROJECT_FILE_CONF $DOCKER_FILE_CONF
+
+      cp .env.example .env
+
+      # shellcheck disable=SC2129
+      # shellcheck disable=SC2028
+      echo "\n" >> .env
+      echo "USERNAME=$(whoami)" >> .env
+      # shellcheck disable=SC2046
+      echo "USERID=$(id -u $(whoami))" >> .env
+
+      docker-compose up --build -d
+
+      docker exec -it $(grep CONTAINER_NAME_API .env | cut -d '=' -f2) sh -c "cd $PROJECT_FOLDER && cp .env.example .env && composer install && php artisan key:generate" # && php artisan migrate && php artisan db:seed
+
+      exit
     fi
     COUNTER=$(( COUNTER + 1 ))
   done
-
 fi
 
-if [ "$1" = "install" ]; then
-  cp .env.example .env
+if [ "$1" = "uninstall" ]; then
+  echo "List of new projects:"; echo;
 
-  # shellcheck disable=SC2129
-  # shellcheck disable=SC2028
-  echo "\n" >> .env
-  echo "USERNAME=$(whoami)" >> .env
-  # shellcheck disable=SC2046
-  echo "USERID=$(id -u $(whoami))" >> .env
+  oldProjectsList
 
-  docker-compose up --build
+  echo; echo "Choose number of project for uninstallation:"
+  read CHOOSED_PROJECT
+
+  COUNTER=0;
+  for project in api/*; do
+    if [ $COUNTER = $CHOOSED_PROJECT ]; then
+      PROJECT_FOLDER=$(echo $project | cut -d '/' -f2)
+      FILE_NAME=$(echo $project | cut -d '/' -f2 | awk '{print tolower($0)}')
+
+      DOCKER_FILE_CONF=docker/sites/$FILE_NAME.conf
+
+      docker-compose down
+
+      [ -f "$DOCKER_FILE_CONF" ] && rm $DOCKER_FILE_CONF
+      [ -d "api/"$PROJECT_FOLDER ] && rm -rf api/$PROJECT_FOLDER
+
+      docker-compose up --build -d
+
+      exit;
+    fi
+    COUNTER=$(( COUNTER + 1 ))
+  done
 fi
