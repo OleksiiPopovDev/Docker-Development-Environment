a166f5b2aae2e80305e91ceb6824d4042727e272
Aleksey Popov <popov.scar@gmail.com>
2020-12-25 15:01:46 +0200

    NGINX configs

--- worker.sh
+++ worker.sh
@@ -1,9 +1,51 @@
+#!/bin/bash
+
 if [ "$1" = "monitor" ]; then
   sampler --config ./docker/sampler/config.yml
 fi
 
 if [ "$1" = "bash" ]; then
-  docker exec -it AlekseyPopov-Dev-Env-api /bin/bash
+  docker exec -it $(grep CONTAINER_NAME_API .env | cut -d '=' -f2) /bin/bash
+fi
+
+if [ "$1" = "start" ]; then
+  docker-compose up --build
+fi
+
+if [ "$1" = "test" ]; then
+  echo "List of new projects:"; echo;
+
+  COUNTER=0;
+  for d in $(cat .repositories); do
+    echo $d
+  done
+  for d in api/*/; do
+    #cp "$d"docker/nginx.conf docker/sites/$(echo $d | cut -d '/' -f2)-test.conf
+    #PROJECT_NAME=$(echo $d | cut -d '/' -f2 | awk '{print tolower($0)}')
+    FILE=docker/sites/$PROJECT_NAME-test.conf
+
+    if [ ! -f "$FILE" ]; then
+      echo $(echo $d | cut -d '/' -f2)" - [$COUNTER]"
+      COUNTER=$(( COUNTER + 1 ))
+    fi
+  done
+
+  echo; echo "Choose number of project for installation:"
+  read CHOOSED_PROJECT
+
+  COUNTER=0;
+  for d in api/*/; do
+    if [ $COUNTER = $CHOOSED_PROJECT ]; then
+      PROJECT_NAME=$(echo $d | cut -d '/' -f2 | awk '{print tolower($0)}')
+      FILE=docker/sites/$PROJECT_NAME-test.conf
+
+      echo;
+      echo "Choosed project: "$PROJECT_NAME
+      echo "Copy conf file: "$FILE
+    fi
+    COUNTER=$(( COUNTER + 1 ))
+  done
+
 fi
 
 if [ "$1" = "install" ]; then
